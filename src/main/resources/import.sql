b3bb77af4c5e687b039c4e720595b305cfdf9264
zenonxd <olavomoreira99@gmail.com>
2024-08-02 10:16:57 -0300

    seed de dados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,9 @@
-INSERT INTO tb_atividade(name, descricao, preco) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.0);
-INSERT INTO tb_atividade(name, descricao, preco) VALUES ('Oficina do Github', 'Controle versões de seus projetos', 50.0);
+INSERT INTO tb_categoria(descricao) VALUES ('Curso');
+INSERT INTO tb_categoria(descricao) VALUES ('Oficina');
+
+INSERT INTO tb_atividade(name, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.0, 1);
+INSERT INTO tb_atividade(name, descricao, preco, categoria_id) VALUES ('Oficina do Github', 'Controle versões de seus projetos', 50.0, 2);
+
 
 INSERT INTO tb_participante(name, email) VALUES ('José Silva', 'jose@gmail.com');
 INSERT INTO tb_participante(name, email) VALUES ('Tiago Faria', 'tiago@gmail.com');
@@ -15,3 +19,13 @@ INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES (3, 
 INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES (3, 2);
 
 INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES (4, 2);
+
+INSERT INTO tb_bloco(atividades_id, inicio, fim) VALUES (1, TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z');
+
+INSERT INTO tb_bloco(atividades_id, inicio, fim) VALUES (2, TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z');
+
+
+INSERT INTO tb_bloco(atividades_id, inicio, fim) VALUES (2, TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z');
+
+
+
